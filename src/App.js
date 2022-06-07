f817acca9f8323634ee8e4df624b2c6f6ee7a8f8
askmary <mariana.reinaldo2003@gmail.com>
2022-06-07 18:43:10 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react"
+import styled from "styled-components"
+import axios from "axios"
+import { createGlobalStyle } from "styled-components";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const GlobalStyle = createGlobalStyle`
+  *{
+    margin:0;
+    padding:0;
+    box-sizing:border-box;
+  }
+  body{
+    width:100%;
+  }
+`
+const Container = styled.section`
+  width:100%;
+  background-color:red;
+  display:flex;
+  flex-direction:column;
+  justify-content:space-evenly;
+  align-items:center;
+
+`
+const SubContainer = styled.div`
+ width:30em;
+ height:35em;
+ background:white;
+ box-shadow:0 8px 32px 0 rgba(31, 38, 135, 0.37);
+ border: 1px solid rgba(255, 255, 255, 0.18);
+ display:flex;
+ justify-content:center;
+`
+const Img = styled.img`
+  width:25em;
+  height:25em;
+  margin-top:3em;
+`
+const Btn = styled.button`
+ border-radius:7px;
+ width:10em;
+ height:2em;
+ font-size:1.5rem;
+ cursor:pointer;
+`
+export default function App(){
+  const [img, setImg] = useState([])
+  function Dog(){
+    axios.get("https://dog.ceo/api/breeds/image/random").then((prevState) => {
+      setImg(prevState.data.message)
+    })
+  }
+  return(
+    <Container>
+      <GlobalStyle/>
+      <h1>Dia ruim?</h1>
+      <SubContainer>
+        <Img src={img} alt="cachorros lindos"/>
+      </SubContainer>
+      <Btn onClick={() => {Dog()}}>Seja feliz!</Btn>
+    </Container>
+  )
 }
-
-export default App;
